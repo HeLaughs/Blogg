15bed457512c8f7093f1a33de178e9e9a4c74ff6
BarasaJnr <isaacbarasa@gmail.com>
2024-09-17 16:38:47 +0300

    update commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import FeaturedPost from '$lib/components/featured.svelte'
+	import Post from '$lib/components/post.svelte'
+	import type { PageData } from './$types'
+
+	export let data: PageData
+
+	$: ({ featured, posts } = data)
+</script>
+
+<div class="container">
+	{#if featured}
+		<FeaturedPost post={featured} />
+	{/if}
+
+	<div class="posts">
+		{#each posts as post}
+			<Post {post} />
+		{/each}
+	</div>
+</div>
+
+<style lang="postcss">
+	.container {
+		--padding-block: var(--space-4);
+
+		@media (--sm) {
+			--padding-block: var(--space-7);
+		}
+	}
+
+	.posts {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+		margin-top: var(--space-7);
+		gap: var(--space-6);
+	}
+</style>
